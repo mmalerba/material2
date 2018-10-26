19301f1b3ea3462a611bb622ffa17bff9eea1109
Miles Malerba <mmalerba@google.com>
2018-10-25 17:04:17 -0700

    basic mdc checkbox

--- src/material-experimental/mdc/checkbox/index.ts
+++ src/material-experimental/mdc/checkbox/index.ts
@@ -6,5 +6,4 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export * from './version';
-export * from './mdc/checkbox/index';
+export * from './public-api';
--- src/material-experimental/mdc/checkbox/public-api.ts
+++ src/material-experimental/mdc/checkbox/public-api.ts
@@ -6,5 +6,5 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
-export * from './version';
-export * from './mdc/checkbox/index';
+export * from './checkbox';
+export * from './module';
