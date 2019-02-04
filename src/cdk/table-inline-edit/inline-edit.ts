9ae48c8a36ef7f021e1576a46765c6e4031f96ec
Karl Seamon <bannanafiend@gmail.com>
2019-02-04 11:54:54 -0500

    wip

--- src/cdk/table-inline-edit/inline-edit.ts
+++ src/cdk/table-inline-edit/inline-edit.ts
@@ -6,6 +6,9 @@ import {audit, debounceTime, filter, first, mapTo, takeUntil, throttle} from 'rx
 const HOVER_DELAY_MS = 50;
 
 export const CDK_INLINE_EDIT_OPENED = new InjectionToken<Subject<boolean>>('cdk_ieo');
+export const CDK_HOVER = new InjectionToken<Subject<boolean>>('cdk_h');
+
+const booleanSubjectFactory = () => new BehaviorSubject<boolean>(false);
 
 @Directive({
   selector: '[cdkInlineEdit]',
@@ -16,7 +19,7 @@ export const CDK_INLINE_EDIT_OPENED = new InjectionToken<Subject<boolean>>('cdk_
   providers: [
     {
       provide: CDK_INLINE_EDIT_OPENED,
-      useFactory: () => new BehaviorSubject<boolean>(false),
+      useFactory: booleanSubjectFactory,
     }
   ]
 })
@@ -68,83 +71,122 @@ export class CdkTableInlineEdit<T> implements OnDestroy {
   }
 }
 
-@Directive({selector: '[cdkCellOverlay]'})
-export class CdkTableCellOverlay implements OnDestroy {
-  @Input() cdkCellOverlay: TemplateRef<any>|null = null;
-  
-  protected readonly opened = new Subject<boolean>();
-  protected readonly activity = new Subject<void>();
+export abstract class Destroyable implements OnDestroy {
   protected readonly destroyed = new ReplaySubject<void>();
-  
-  protected overlayRef?: OverlayRef;
-  
-  constructor(
-      protected readonly elementRef: ElementRef,
-      protected readonly overlay: Overlay,
-      ngZone: NgZone) {
-    
-    ngZone.runOutsideAngular(() => {
-      const opened = this.opened.pipe(takeUntil(this.destroyed));
-      opened.subscribe(this.activity);
-      
-      opened.pipe(
-          audit(() => this.activity.pipe(
-              takeUntil(this.destroyed),
-              debounceTime(HOVER_DELAY_MS),)),
-          distinctUntilChanged(),)
-          .subscribe((open) => {
-            ngZone.run(() => {
-              if (open) {
-                if (!this.overlayRef) {
-                  // TODO: work out details of positioning over cell.
-                  this.overlayRef = overlay.create({
-                    positionStrategy: overlay.position().flexibleConnectedTo(elementRef),
-                    scrollStrategy: overlay.scrollStrategies.reposition({autoClose: true}),
-                  });
-                  
-                  ngZone.runOutsideAngular(() => {
-                    this.connectEvents(this.overlayRef.overlayElement());
-                  });
-                }
-        
-                // TODO: Is it better to create a portal once and reuse it?
-                this.overlayRef.attach(new TemplatePortal(this.cdkInlineEditCellOverlay));
-              } else if (this.overlayRef) {
-                this.overlayRef.detach();
-              }
-            });
-          });
-      
-      this.connectEvents(elementRef);
-    });
-  }
   
   ngOnDestroy() {
     this.destroyed.next();
     this.destroyed.complete();
   }
+}
+
+@Directive({
+  selector: '[cdkRowHover]',
+  providers: providers: [
+    {
+      provide: CDK_HOVER,
+      useFactory: booleanSubjectFactory,
+    }
+  ]
+})
+export class CdkTableRowHover extends Destroyable {
+  constructor(
+      elementRef: ElementRef,
+      @Inject(CDK_HOVER) protected readonly hoverSubject,
+      ngZone: NgZone) {
+    connectHoverEvents(elementRef, this.destroyed, ngZone, hoverSubject);
+  }
+}
+
+@Directive({selector: '[cdkCellOverlay]'})
+export class CdkTableCellOverlay extends Destroyable {
+  @Input() cdkCellOverlay: TemplateRef<any>|null = null;
   
-  protected connectEvents(elementRef: ElementRef) {
-    const enter = fromEvent(elementRef.nativeElement!, 'mouseenter')
-        .pipe(mapTo(true));
-    enter.subscribe(this.opened);
+  protected readonly hoverSubject: Subject<boolean>;
+
+  protected overlayRef?: OverlayRef;
+  
+  constructor(
+      elementRef: ElementRef,
+      @Optional() @Inject(CDK_HOVER) rowHoverSubject: Subject<boolean>,
+      overlay: Overlay,
+      ngZone: NgZone) {
+    if (rowHoverSubject) {
+      this.hoverSubject = rowHoverSubject;
+    } else {
+      this.hoverSubject = new Subject<boolean>();
+      connectHoverEvents(elementRef, this.destroyed, ngZone, hoverSubject);
+    }
     
-    // Optimization: Defer registration of other mouse events until first enter.
-    enter
-        .pipe(
-            takeUntil(this.destroyed),
-            first(),)
-        .subscribe(() => {
-          fromEvent(elementRef.nativeElement!, 'mouseleave')
-              .pipe(mapTo(false))
-              .subscribe(this.opened);
-          fromEvent(elementRef.nativeElement!, 'mousemove')
-              .subscribe(this.activity);
+    this.hoverSubject
+        .subscribe((open) => {
+            if (open) {
+              if (!this.overlayRef) {
+                // TODO: work out details of positioning over cell.
+                this.overlayRef = overlay.create({
+                  positionStrategy: overlay.position().flexibleConnectedTo(elementRef),
+                  scrollStrategy: overlay.scrollStrategies.reposition({autoClose: true}),
+                });
+                
+                ngZone.runOutsideAngular(() => {
+                  this.connectEvents(this.overlayRef.overlayElement());
+                });
+              }
+      
+              // TODO: Is it better to create a portal once and reuse it?
+              this.overlayRef.attach(new TemplatePortal(this.cdkInlineEditCellOverlay));
+            } else if (this.overlayRef) {
+              this.overlayRef.detach();
+            }
         });
   }
 }
 
+function connectHoverEvents(
+    elementRef: ElementRef,
+    destroyed: Observable<void>,
+    ngZone: ngZone,
+    outputSubject: Subject<boolean>) {
+      ngZone.runOutsideAngular(() => {
+        const activity = new Subject<boolean>();
+        const opened = new BehaviorSubject(false);
+
+        const openedUntilDestroyed = opened.pipe(takeUntil(destroyed));
+        openedUntilDestroyed.subscribe(activity);
+    
+        openedUntilDestroyed.pipe(
+            audit(() => activity.pipe(
+                takeUntil(destroyed),
+                debounceTime(HOVER_DELAY_MS),)),
+            distinctUntilChanged(),)
+            .subscribe((open) => {
+              ngZone.run(() => {
+                outputSubject.next(open);
+              });
+            });
+      });
+      
+      const enter = fromEvent(elementRef.nativeElement!, 'mouseenter')
+          .pipe(mapTo(true));
+      enter.subscribe(opened);
+  
+      // Optimization: Defer registration of other mouse events until first enter.
+      enter
+          .pipe(
+              takeUntil(destroyed),
+              first(),)
+          .subscribe(() => {
+            fromEvent(elementRef.nativeElement!, 'mouseleave')
+                .pipe(mapTo(false))
+                .subscribe(opened);
+            fromEvent(elementRef.nativeElement!, 'mousemove')
+                .subscribe(activity);
+          });
+}
+
 // TODO: move to a separate file
+// TODO: will this work from inside the popup? probably need to come up with something
+// akin to getClosestDialog to find the opened subject
 @Directive({
   selector: 'button[cdkInlineEditOpen]',
   host: {
