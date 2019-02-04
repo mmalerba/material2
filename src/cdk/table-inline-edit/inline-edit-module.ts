fc0e5d9f7c021d53dcb07ef65e45500b2364fdf5
Karl Seamon <bannanafiend@gmail.com>
2019-02-04 13:42:42 -0500

    ready to try building

---
+++ src/cdk/table-inline-edit/inline-edit-module.ts
@@ -0,0 +1,23 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+/*import {CommonModule} from '@angular/common';*/
+import {NgModule} from '@angular/core';
+import {CdkTableInlineEdit, CdkTableRowHover, CdkTableCellOverlay, CdkTableInlineEditOpen} from './inline-edit';
+
+const EXPORTED_DECLARATIONS = [
+  CdkTableInlineEdit, CdkTableRowHover, CdkTableCellOverlay, CdkTableInlineEditOpen,
+];
+
+@NgModule({
+/*  imports: [CommonModule],*/
+  exports: EXPORTED_DECLARATIONS,
+  declarations: EXPORTED_DECLARATIONS
+
+})
+export class CdkTableInlineEditModule { }
--- src/cdk/table-inline-edit/inline-edit.ts
+++ src/cdk/table-inline-edit/inline-edit.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
 import {Directive, ElementRef, HostListener, Inject, Injectable, Input, OnDestroy, Optional, NgZone, TemplateRef} from '@angular/core';
 import {Overlay, OverlayRef} from '@angular/cdk/overlay';
 import {BehaviorSubject, fromEvent, Observable, ReplaySubject, Subject, Subscription} from 'rxjs';
