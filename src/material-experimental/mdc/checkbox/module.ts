953629a444c3556637ea6c4a7d48013328bc069b
Miles Malerba <mmalerba@google.com>
2019-02-15 10:40:28 -0800

    use query-able mixins for theme

--- src/material-experimental/mdc/checkbox/module.ts
+++ src/material-experimental/mdc/checkbox/module.ts
@@ -6,13 +6,14 @@
  * found in the LICENSE file at https://angular.io/license
  */
 
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {MatCommonModule} from '@angular/material/core';
 import {MatMdcRippleModule} from '../ripple/module';
 import {MatMdcCheckbox} from './checkbox';
 
 @NgModule({
-  imports: [MatCommonModule, MatMdcRippleModule],
+  imports: [MatCommonModule, MatMdcRippleModule, CommonModule],
   exports: [MatMdcCheckbox, MatCommonModule],
   declarations: [MatMdcCheckbox],
 })
