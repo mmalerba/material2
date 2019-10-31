9ef0e5baafa1de2508a3a3e059dd82005862fa57
Miles Malerba <mmalerba@google.com>
2019-10-31 12:17:08 -0700

    repro stepper issue

--- src/dev-app/stepper/stepper-demo-module.ts
+++ src/dev-app/stepper/stepper-demo-module.ts
@@ -12,6 +12,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
 import {MatStepperModule} from '@angular/material/stepper';
 import {RouterModule} from '@angular/router';
@@ -27,6 +28,7 @@ import {StepperDemo} from './stepper-demo';
     MatInputModule,
     MatStepperModule,
     ReactiveFormsModule,
+    MatIconModule,
     RouterModule.forChild([{path: '', component: StepperDemo}]),
   ],
   declarations: [StepperDemo],
--- src/dev-app/stepper/stepper-demo.ts
+++ src/dev-app/stepper/stepper-demo.ts
@@ -15,47 +15,6 @@ import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/form
   selector: 'stepper-demo',
   templateUrl: 'stepper-demo.html',
 })
-export class StepperDemo implements OnInit {
-  formGroup: FormGroup;
-  isNonLinear = false;
-  isNonEditable = false;
-  disableRipple = false;
-
-  nameFormGroup: FormGroup;
-  emailFormGroup: FormGroup;
-
-  steps = [
-    {label: 'Confirm your name', content: 'Last name, First name.'},
-    {label: 'Confirm your contact information', content: '123-456-7890'},
-    {label: 'Confirm your address', content: '1600 Amphitheater Pkwy MTV'},
-    {label: 'You are now done', content: 'Finished!'}
-  ];
-
-  /** Returns a FormArray with the name 'formArray'. */
-  get formArray(): AbstractControl | null { return this.formGroup.get('formArray'); }
-
-  constructor(private _formBuilder: FormBuilder) { }
-
-  ngOnInit() {
-    this.formGroup = this._formBuilder.group({
-      formArray: this._formBuilder.array([
-        this._formBuilder.group({
-          firstNameFormCtrl: ['', Validators.required],
-          lastNameFormCtrl: ['', Validators.required],
-        }),
-        this._formBuilder.group({
-          emailFormCtrl: ['', Validators.email]
-        }),
-      ])
-    });
-
-    this.nameFormGroup = this._formBuilder.group({
-      firstNameCtrl: ['', Validators.required],
-      lastNameCtrl: ['', Validators.required],
-    });
-
-    this.emailFormGroup = this._formBuilder.group({
-      emailCtrl: ['', Validators.email]
-    });
-  }
+export class StepperDemo {
+  shouldShowStep2 = false;
 }
