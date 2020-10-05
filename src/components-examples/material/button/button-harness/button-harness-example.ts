cd2a05fc022b2d5327e91e8a33cde0aa4525b0f8
Annie Wang <annieyw@google.com>
2020-10-05 13:01:32 -0400

    docs(material/button): add button harness example

---
+++ src/components-examples/material/button/button-harness/button-harness-example.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Button harness
+ */
+@Component({
+  selector: 'button-harness-example',
+  templateUrl: 'button-harness-example.html',
+})
+export class ButtonHarnessExample  {
+  clicked = false;
+}
